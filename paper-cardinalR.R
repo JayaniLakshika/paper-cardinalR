e441f62a3a7bca1133573cd99d90e6450618b6a2
JayaniLakshika <jayanilakshika76@gmail.com>
2025-10-08 13:16:58 +1100

    Added multicluster code example

--- paper-cardinalR.R
+++ paper-cardinalR.R
@@ -124,6 +124,38 @@ main_tb |>
   column_spec(3, width = "8cm")
 
 
+## ----echo=TRUE----------------------------------------------------------------
+# Define example rotation matrices for 4D space
+rot1 <- gen_rotation(p = 4, planes_angles = list(
+  list(plane = c(1, 2), angle = 60),
+  list(plane = c(3, 4), angle = 90)
+))
+
+rot2 <- gen_rotation(p = 4, planes_angles = list(
+  list(plane = c(1, 3), angle = 30)
+))
+
+rot3 <- gen_rotation(p = 4, planes_angles = list(
+  list(plane = c(2, 4), angle = 45)
+))
+
+# Generate the clustered dataset
+clust_data <- gen_multicluster(
+  n = c(200, 300, 500),
+  k = 3,
+  loc = matrix(c(
+    0, 0, 0, 0,
+    5, 9, 0, 0,
+    3, 4, 10, 7
+  ), nrow = 3, byrow = TRUE),
+  scale = c(3, 1, 2),
+  shape = c("gaussian", "cone", "unifcube"),
+  rotation = list(rot1, rot2, rot3),
+  is_bkg = FALSE
+)
+
+
+
 ## -----------------------------------------------------------------------------
 branch_tb <- tibble(fun = c("gen_expbranches",
                              "gen_linearbranches",
