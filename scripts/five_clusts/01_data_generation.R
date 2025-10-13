fb391eb1277d4c1fbb7f200545e99e5cba76a6b5
JayaniLakshika <jayanilakshika76@gmail.com>
2025-10-13 12:54:27 +1100

    Regenerate embeddings with new data

--- scripts/five_clusts/01_data_generation.R
+++ scripts/five_clusts/01_data_generation.R
@@ -3,15 +3,16 @@ library(readr)
 set.seed(20240412)
 
 positions <- geozoo::simplex(p=4)$points
-positions <- positions * 0.8
+positions <- positions * 0.3
 
 ## To generate data
-five_clusts <- gen_multicluster(n = c(2250, 1500, 750, 1250, 1750), p = 4, k = 5,
-                       loc = positions,
-                       scale = c(0.4, 0.35, 0.3, 1, 0.3),
-                       shape = c("helicalspiral", "hemisphere", "unifcube", "bluntedcone", "gaussian"),
-                       rotation = NULL,
-                       is_bkg = FALSE)
+five_clusts <- gen_multicluster(n = c(2250, 1500, 750, 1250, 1750), k = 5,
+                                loc = positions,
+                                scale = c(0.25, 0.35, 0.3, 1, 0.3),
+                                shape = c("helicalspiral", "hemisphere", "unifcube",
+                                          "cone", "gaussian"),
+                                rotation = NULL,
+                                is_bkg = FALSE)
 
 langevitour::langevitour(five_clusts[, -5])
 
